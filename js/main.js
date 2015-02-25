534845bcac60e9eac28b48563d9914d1dcc4edc2
dock70 <rubencosta@MacBook-Pro.local>
2015-02-25 01:18:24 +0000

    GRID FINAL

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
 
 $(document).ready(function(){
+
+	//GATEDOOR//
 	$("#gateDoor ul li:first-child").click(function(e){
 		e.preventDefault();
 		$("#gateDoor").hide();
 	});
+	//END GATEDOOR
 
-	var $container = $('#container');
-	// init
+	//ISOTOPE
+	var $container = $('.isotope');
 	$container.isotope({
-	  // options
 	  itemSelector: '.item',
-	  layoutMode: 'masonry'
+	  masonry: {
+	  	columnWidth: 100,
+	  	gutterWidth: 30,
+	  	isFitWidth: true,
+	  },
+  	});
+		// sort items on button click
+	$('#filters a').click(function() {
+    	var filterValue = $(this).attr('data-filter');
+    	if (filterValue == '*') {
+    		$container.isotope({ filter: filterValue });
+    	}else{
+    		$container.isotope({ filter: '.'+filterValue });
+    	}
+    	$('#filters a').removeClass('active');
+    	$(this).addClass('active');
 	});
 
+	//END ISOTOPE
+	$(window).resize(function(){
+
+        if ($(window).width() <= 767) {  
+       		var $container = $('.isotope');
+			$container.isotope({
+			  masonry: {
+			  	isFitWidth: true,
+			  },
+			});
+
+       }     
+
+	});
 });
 
 
